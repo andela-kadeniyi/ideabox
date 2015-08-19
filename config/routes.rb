a53a60a687185b4ceaace052301ebeb92fb782fe
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-08-19 11:01:40 +0100

    Added api version to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+ namespace :api do
+  namespace :v1 do  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,4 +60,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  end
+ end
 end
